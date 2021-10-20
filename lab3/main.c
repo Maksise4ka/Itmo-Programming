53773cb9e2885d11f52b157c95fc76f734171db9
Max <89832699+Maksise4ka@users.noreply.github.com>
2021-10-21 00:15:41 +0300

    Answer on first question

--- lab3/main.c
+++ lab3/main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <string.h>
 #include "allpos.h"
@@ -23,25 +22,4 @@ int main(){
     printf("%d", k);
     fclose(file);
     return 0;
-=======
-#include <stdio.h>
-#include <string.h>
-#include "allpos.h"
-
-
-int main(){
-    FILE *file;
-    file = fopen("access_logmini", "r");
-    
-
-    char str[500];
-    while (!feof(file)){
-        fgets(str, 500, file);
-        //printf("%s\n", str);
-        allpos pos = split(str);
-    }
-
-    fclose(file);
-    return 0;
->>>>>>> 2f1ca87490c29982f659c171c74eb2254b7df6a2
 }
