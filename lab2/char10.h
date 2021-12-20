590a6969f4720baebd7552676a2d970ebb321349
Max <for_maxim@bk.ru>
2021-12-20 21:58:48 +0300

    move funcs from header

--- lab2/char10.h
+++ lab2/char10.h
@@ -6,62 +6,14 @@ typedef struct char10{
     int index, size;
 } char10;
 
-char10 init10(){
-    char10 num;
-    num.index = 0;
-    num.size = 309;
-    num.n = (char *) malloc(311 * sizeof(char));
-    memset(num.n, 0, 311);
-    return num;
-}
+char10 init10();
 
-void free_char10(char10 x){
-    free(x.n);
-}
+void free_char10(char10);
 
-char10 add10(char10 x, char10 y){
-    char10 result = init10();
-    int prev = 0;
-    for(int i = 0; i <= 309; ++i){ 
-        result.n[i] = x.n[i] + y.n[i] + prev;
+char10 add10(char10, char10);
 
-        if (result.n[i] >= 10){
-            result.n[i] %= 10;
-            prev = 1;
-        }
-        else
-            prev = 0;
+int find10(char10);
 
-        if(result.n[i])
-            result.index = i;
-    }
+void printf10(char10);
 
-    /*if (prev == 1){
-        printf("UB");
-        return init10();
-    }*/
-    return result;
-}
-
-int find10(char10 x){
-    for(int i = 309; i >= 0; --i){
-        int cur = x.n[i];
-        if (cur)
-            return i;
-    }
-    return 0;
-}
-
-void printf10(char10 x){
-    x.index = find10(x);
-    for(int i = x.index; i >= 0; --i)
-        printf("%d", x.n[i]);
-    printf("\n");
-}
-
-void fprintf10(char10 x, FILE *file){
-    x.index = find10(x);
-    for(int i = x.index; i >= 0; --i)
-        fprintf(file, "%d", x.n[i]);
-    fprintf(file, " ");
-}
+void fprintf10(char10, FILE*);
